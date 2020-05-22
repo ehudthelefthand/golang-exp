c0539fa6b0d121775e895682120a689a3e578b20
Peerawat Poombua <peerawat@odds.team>
2020-05-22 16:32:38 +0700

    added file handler

--- gin/main.go
+++ gin/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"fmt"
+	"log"
+	"github.com/gin-gonic/gin"
 )
 
 
@@ -27,7 +28,54 @@ func main() {
 		})
 	})
 
-	// r.POST("/login")
+	r.POST("/login", func (c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+		c.JSON(200, gin.H{
+			"username": username,
+			"password": password,
+		})
+	})
+
+	r.POST("/login2", func (c *gin.Context) {
+		type User struct {
+			Username string 
+			Password string
+		}
+		user := User{}
+		err := c.BindJSON(&user)
+		if err != nil {
+			c.Status(400)
+			return
+		}
+		log.Printf("%v\n", user)
+		c.JSON(200, gin.H{
+			"message": "ok",
+		})
+	})
+
+	r.POST("/login3", func (c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+		form, err := c.MultipartForm()
+		if err != nil {
+			c.Status(400)
+			return
+		}
+		log.Printf("%v\n", username)
+		log.Printf("%v\n", password)
+		log.Printf("%v\n", form.File["photo"])
+		file := form.File["photo"][0]
+		err = c.SaveUploadedFile(file, 
+			"./upload/" + file.Filename)
+		if err != nil {
+			c.Status(400)
+			return
+		}
+		c.JSON(200, gin.H{
+			"message": "ok",
+		})
+	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
